62713a3fc4d58fc4e6c03d28ea48bcda0e13bfd1
mustafaertekin <mustafa014@gmail.com>
2018-10-19 19:04:00 +0200

    test

--- client/src/app/services/app-http.service.ts
+++ client/src/app/services/app-http.service.ts
@@ -9,8 +9,8 @@ export class AppHttpService {
   private headers: HttpHeaders;
   private serverUrl: string;
   constructor(private http: HttpClient, private tokenService: TokenService) {
-    // this.serverUrl = 'http://localhost:3001/';
-    this.serverUrl = 'http://104.248.32.52:3001/';
+    this.serverUrl = 'http://localhost:3001/';
+    // this.serverUrl = 'http://104.248.32.52:3001/';
     this.createAuthorizationHeader();
   }
 
