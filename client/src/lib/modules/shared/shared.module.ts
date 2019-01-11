ff8c63218ae5569746245d4ee624221d00d90433
AliOgretmen <aliogretmen.ch@gmail.com>
2019-01-11 23:10:46 +0100

    translate added into order comp

--- client/src/lib/modules/shared/shared.module.ts
+++ client/src/lib/modules/shared/shared.module.ts
@@ -4,11 +4,24 @@ import { GeneralErrorComponent } from './general-error-message/general-error.com
 import { AvsLineChartComponent } from './graphs/line-chart/line-chart.component';
 import { FadeInComponent } from './animations/fadein/fadein.component';
 import { MaterialModule } from '@avs-ecosystem/modules/material/material.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   imports: [
-    MaterialModule
+    MaterialModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+    })
   ],
   declarations: [
     SearchComponent,
